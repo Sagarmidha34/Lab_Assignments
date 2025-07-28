e79a6041aea297445ceaed3d4b573b4e4d9cf831
Sagarmidha34 <sagar1197.be23@chitkara.edu.in>
2025-07-28 10:12:57 +0530

    initial commit

---
+++ Guided_Apple_Pie/Guided_Apple_PieTests/Guided_Apple_PieTests.swift
@@ -0,0 +1,17 @@
+//
+//  Guided_Apple_PieTests.swift
+//  Guided_Apple_PieTests
+//
+//  Created by Sagar Midha on 22/07/25.
+//
+
+import Testing
+@testable import Guided_Apple_Pie
+
+struct Guided_Apple_PieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Guided_Apple_Pie/Guided_Apple_PieUITests/Guided_Apple_PieUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Guided_Apple_PieUITestsLaunchTests.swift
+//  Guided_Apple_PieUITests
+//
+//  Created by Sagar Midha on 22/07/25.
+//
+
+import XCTest
+
+final class Guided_Apple_PieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
