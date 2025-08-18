dd799476a35da88f1916d53701b9bf268b27cd30
Sagarmidha34 <sagar1197.be23@chitkara.edu.in>
2025-08-18 11:49:29 +0530

    Meal Tracker
    lab of tabel view

---
+++ ST1_Practice/ST1_Practice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ST1_Practice
+//
+//  Created by Sagar Midha on 07/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindTofrst(_ unwindSegue: UIStoryboardSegue) {
+      
+    }
+
+}
+
