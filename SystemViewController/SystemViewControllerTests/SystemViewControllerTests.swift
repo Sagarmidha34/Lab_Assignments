4dd9c08640a71663f864ec19a9ac4ef1c1c81a47
Sagarmidha34 <sagar1197.be23@chitkara.edu.in>
2025-08-29 11:53:15 +0530

    system view controller and its lab

---
+++ SystemViewController/SystemViewControllerTests/SystemViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SystemViewControllerTests.swift
+//  SystemViewControllerTests
+//
+//  Created by Sagar Midha on 29/08/25.
+//
+
+import Testing
+@testable import SystemViewController
+
+struct SystemViewControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
